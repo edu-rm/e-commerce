9164362efac3ec48546ff90da3c2bca9bae99488
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-08 00:02:06 -0300

    Basic form structure created

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const routes = express.Router() /* Variavel resposavel pelas rotas */
-
+const ProductController = require("./app/controllers/ProductController")
 routes.get('/', function(req,res){
   res.render("layout.njk")
+})
+
+routes.get('/products/create', ProductController.create )
+
+routes.get('/ads/create', function(req, res){
+  return res.redirect("/products/create")
 })
 
 
