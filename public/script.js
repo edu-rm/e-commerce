102afb2d0389b2e7ca1a0ab4dcdfd2e1083d4b08
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-08 17:01:39 -0300

    money mask on price created (product create form)

---
+++ public/script.js
@@ -0,0 +1,18 @@
+
+const Mask = {
+  apply(input, func){
+    setTimeout(function(){
+      input.value = Mask[func](input.value)
+    },1)
+  },
+  formatBRL(value){
+
+    value = value.replace(/\D/g,"")
+
+    return value = new Intl.NumberFormat('pt-BR', {
+      style : 'currency',
+      currency : 'BRL'
+    }).format(value/100)
+
+  } 
+}
