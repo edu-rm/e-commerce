a192cc3dc43338c2ea182eebbc5f56c4e69b4e26
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-11 19:27:39 -0300

    Getting and reading photo uploaded for show it at photos preview div

--- public/script.js
+++ public/script.js
@@ -16,3 +16,41 @@ const Mask = {
 
   } 
 }
+
+const PhotosUpload = {
+  uploadLimit : 6,
+  handleFileInput(event){
+    const {files : fileList } = event.target
+    
+    const { uploadLimit } = PhotosUpload
+    const photoPreview = document.querySelector('#photos-preview')
+    
+    if(fileList.length > uploadLimit){
+      alert(`Você pode enviar até ${uploadLimit} fotos`)
+      event.preventDefault()
+      return
+    }
+    Array.from(fileList).forEach(file =>{
+      const reader = new FileReader()
+
+      reader.onload = ()=>{
+        const img = new Image()/*<img> */
+        img.src = String(reader.result)
+
+        const div = document.createElement('div')
+        div.classList.add('photo')
+
+        div.onclick = () => alert('cliquei')
+        
+        div.appendChild(img)
+        photoPreview.appendChild(div)
+
+      }
+
+      reader.readAsDataURL(file)
+    })
+
+  }
+}
+
+
