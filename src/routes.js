dd60ef7be922614d653b7d7f72257e041015bb72
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-12 19:52:54 -0300

    multer function created

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,21 @@
 const express = require("express")
 const routes = express.Router() /* Variavel resposavel pelas rotas */
+const multer = require('./app/middlewares/multer')
 const ProductController = require("./app/controllers/ProductController")
+
+
+
 routes.get('/', function(req,res){
   res.render("layout.njk")
 })
 
 routes.get('/products/create', ProductController.create )
-routes.post('/products', ProductController.post)
-routes.get('/products/:id/edit', ProductController.edit)
+routes.post('/products', multer.array["photos", 6], ProductController.post)
+routes.get('/products/:id/edit',multer.array["photos", 6], ProductController.edit)
 routes.put('/products', ProductController.put)
 routes.delete('/products', ProductController.delete)
+
+
 //alias
 routes.get('/ads/create', function(req, res){
   return res.redirect("/products/create")
