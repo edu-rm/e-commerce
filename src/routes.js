81fec900d2e514ab1631068ceafbb1ca1612281c
EduardoRamponMeireles <eduardomeireles3232@gmail.com>
2020-03-18 20:30:56 -0300

    show product for sale page created and stylized

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ const ProductController = require("./app/controllers/ProductController")
 routes.get('/', function(req,res){
   res.render("layout.njk")
 })
-
+routes.get('/products/:id', ProductController.show )
 routes.get('/products/create', ProductController.create )
 routes.post('/products', multer.array("photos", 6), ProductController.post)
 routes.put('/products', multer.array("photos", 6),ProductController.put)
